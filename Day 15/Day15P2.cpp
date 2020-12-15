e2deebc738dc7de07d0588fd423b24fd619c3f40
Wong-ZZ <wongzhengzhi@gmail.com>
2020-12-15 13:46:37 +0800

    Add Day 15 solutions

---
+++ Day 15/Day15P2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> input = {9,12,1,4,17,0,18};
+    unordered_map<int,int> history;
+    int turn = 0, next, last;
+    for (auto& in : input) {
+        history[in] = ++turn;
+    }
+
+    next = 0;
+
+    while (turn < 30000000) {
+        if (turn == 30000000 - 1) cout << next << "\n";
+        ++turn;
+        if (history.count(next) != 1) {
+            history[next] = turn;
+            next = 0;
+            continue;
+        }
+
+        last = next;
+        next = turn - history[last];
+        history[last] = turn;
+    }
+
+    return 0;
+}
